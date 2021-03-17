af746d72db18ebb1ed68880d24e1eafbd18363e8
mohamedmabrouk <mabroukmohamed582@gmail.com>
2021-03-17 12:18:29 +0200

    pic multi pic

--- picker/src/main/java/ir/shahabazimi/instagrampicker/gallery/CameraAdapter.kt
+++ picker/src/main/java/ir/shahabazimi/instagrampicker/gallery/CameraAdapter.kt
@@ -6,10 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.net.toUri
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import ir.shahabazimi.instagrampicker.R
-import ir.shahabazimi.instagrampicker.databinding.CameraItemViewBinding
 import java.io.File
 
 class CameraAdapter(val listener:CameraListener) : RecyclerView.Adapter<CameraAdapter.Holder>() {
